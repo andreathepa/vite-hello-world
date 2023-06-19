2988aada285ed3c52c1eed0542c86ee2f7cf4d87
andreathepa <andreadepa32@gmail.com>
2023-06-19 17:33:11 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import titlehello from './components/titlehello.vue';
+
+export default {
+  components:{
+    titlehello
+  }
+}
+</script>
+<template>
+  <div>
+    <titlehello/>
+  </div>
+</template>
+<style>
+  
+</style>
---
+++ src/components/titlehello.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+<template lang="">
+  <div>
+    <h1>hello in vite world</h1>
+  </div>
+</template>
+<style lang="">
+  
+</style>
